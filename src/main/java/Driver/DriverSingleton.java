d93148085f1cb28525da642a169c2f8fdb089af7
Katerina Stalchenko <katerina.stalchenko@hiqo-solutions.com>
2019-10-08 13:38:49 +0300

    Update

--- src/main/java/Driver/DriverSingleton.java
+++ src/main/java/Driver/DriverSingleton.java
@@ -1,13 +1,12 @@
 package Driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class DriverSingleton {
-    private static final String CHROME_DRIVER = "webdriver.chrome.driver";
-    private static final String PATH_CHROME_DRIVER = "C:\\Data\\Drivers\\chromedriver_win32\\chromedriver.exe";
 
     private static WebDriver driver;
 
@@ -16,7 +15,7 @@ public class DriverSingleton {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.setProperty(CHROME_DRIVER, PATH_CHROME_DRIVER);
+            WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
             driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
             driver.manage().window().maximize();
